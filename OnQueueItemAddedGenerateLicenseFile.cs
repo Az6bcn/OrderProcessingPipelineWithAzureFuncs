d3f9121c1fac8f69b23360f6ead1aee6133f4d75
A.O Odumosu <az6bcn@gmail.com>
2020-02-09 01:57:16 +0000

    Queue triggered Function to process items in queue when a a new item is added to the queue

---
+++ OnQueueItemAddedGenerateLicenseFile.cs
@@ -0,0 +1,27 @@
+using System;
+using System.IO;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+using OrderProcessingPipelineWithAzureFuncs.Models;
+
+namespace OrderProcessingPipelineWithAzureFuncs
+{
+    public static class OnQueueItemAddedGenerateLicenseFile
+    {
+        [FunctionName("GenerateLicenseFile")]
+        public static void Run([QueueTrigger("orders", Connection = "AzureWebJobsStorage")] Order order, ILogger log,
+            [Blob("licenses/{rand-guid}.lic")] TextWriter outputBlob)
+        {
+            // when message is added to the orders queue this function is triggered and generates a license file for the added item in the blob storage.
+            log.LogInformation($"Generating License for order : {order.OrderId}");
+
+            outputBlob.WriteLine($"OrderId: {order.OrderId}");
+            outputBlob.WriteLine($"Email: {order.Email}");
+            outputBlob.WriteLine($"ProductId: {order.ProductId}");
+            outputBlob.WriteLine($"PurchasedDate: {DateTime.UtcNow}");
+
+            log.LogInformation($"Generating License for order : {order.OrderId}");
+        }
+    }
+}
